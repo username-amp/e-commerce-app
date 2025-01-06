d1224495d9fa6446dd1cf6c8f30a6ad8d665734a
Johnravee <johnravemimayn@gmail.com>
2025-01-07 01:00:57 +0800

    `Removed LuCodesandbox logo and replaced with LogoHeader component`

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,15 +1,11 @@
 import { LoginForm } from "@/components/ui/login-form"
-import { LuCodesandbox } from "react-icons/lu"
+import { LogoHeader } from "@/components/ui/header"
 
 export default function Page() {
   return (
     <div className="h-screen">
-      <div className=" flex flex-row items-start justify-start lg:min-w-min h-0  self-start bg-[#0339D9] p-5 ">
-        <LuCodesandbox size={30} className="text-white"/>
-        <h1 className=" decoration-4 text-white text-xl ml-1 font-bold">Techzone.</h1>
-      </div>
+      <LogoHeader />
       <div className="flex flex-col h-[90%] w-full items-center justify-center p-6 md:p-10 bg-[#0339D9]">
-      
       <div className="w-full max-w-sm self-end lg:mr-[10rem] md:self-end sm:self-center mr-0 ">
         <LoginForm />
       </div>
