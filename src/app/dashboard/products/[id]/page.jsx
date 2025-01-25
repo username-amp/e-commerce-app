b9a9596b70741a501af134676fb74c813d9cf9b1
Johnravee <johnravemimayn@gmail.com>
2025-01-25 21:40:09 +0800

    Dynamic route content added

--- src/app/dashboard/products/[id]/page.jsx
+++ src/app/dashboard/products/[id]/page.jsx
@@ -1,7 +1,39 @@
-
+import UserDashboardHeader from "@/components/ui/header";
+import Image from "next/image";
 export default function Page({ params }) {
  
   const { id } = params;
 
-  return <p>Post: {id}</p>;
+  return (
+    <div>
+        <UserDashboardHeader />
+
+
+        <div className="flex h-screen w-full">
+            <div className=" bg-gray-500 h-100 w-full">
+                <div className="flex flex-row">
+                    {/* product image  */}
+                    <div className="flex flex-col">
+                      <Image
+                        src={'/productImg.png'}
+                        alt="product"
+                        width={500}
+                        height={500}
+                        objectFit="cover"
+                      />
+
+                      {/* Carousel */}
+                      <div>
+                        <h1>carousel</h1>
+                      </div>
+                    </div>
+                    {/* buying details */}
+                    <div className="flex">
+                      
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
 }
